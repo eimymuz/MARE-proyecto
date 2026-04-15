0a32416c1410d716e32850727665e964308eaab4
eimymuz <eimymelissa@gmail.com>
2026-04-15 17:30:23 -0600

    style: landing.html

---
+++ apps/embarcaciones/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts               import  render
+
+
+# La gestión de clientes se realiza a través del formulario público
+# (apps/publico) y el admin de Django (/admin/).
+# No se implementó un CRUD propio para clientes en este proyecto.
