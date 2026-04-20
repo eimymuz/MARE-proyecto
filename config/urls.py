baff0c913b3703899b1f710c2b754cc70c2c25ad
Avilix <jimenezavilix@gmail.com>
2026-04-19 23:03:43 -0600

    feat: agregar funcionalidad de mapa en navbar y plantillas
    - Actualizar navbar para enlazar a la página del mapa
    - Cambiar botón "Ver mapa completo" en inicio.html para redirigir al mapa
    - Crear plantilla map.html para mostrar el mapa operativo
    - Agregar map_component.html para la interfaz y controles del mapa
    - Implementar JavaScript para interacciones del mapa (filtros y asignación de espacios)
    - Actualizar solicitud_detail.html y solicitud_list.html para enlazar al mapa con solicitud_id

--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
 
     # Apps
     path('solicitudes/',   include('apps.solicitudes.urls')),
-    #path('muelles/',       include('apps.muelles.urls')),
-    #path('mapa/',          include('apps.mapa.urls')),
+    path('muelles/',       include('apps.muelles.urls')),
+    path('mapa/',          include('apps.mapa.urls')),
     path('asignaciones/',  include('apps.asignaciones.urls')),
     path('clientes/',      include('apps.clientes.urls')),
     path('embarcaciones/', include('apps.embarcaciones.urls')),
